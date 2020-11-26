1a1fb8b65f734af1a99f177e3a1058b7221fbd5a
John15321 <56229312+John15321@users.noreply.github.com>
2020-11-26 02:09:16 +0100

    .

--- Skrypty/rotations.py
+++ Skrypty/rotations.py
@@ -17,7 +17,7 @@ def rot_z_fun(alpha):
 
 def rot_x_fun(beta):
     return sp.Matrix([
-        [1,  0,             0           ],
+        [1,  0,             0],
         [0,  sp.cos(beta), -sp.sin(beta)],
         [0,  sp.sin(beta),  sp.cos(beta)]
     ])
@@ -25,7 +25,14 @@ def rot_x_fun(beta):
 
 def rot_y_fun(gamma):
     return sp.Matrix([
-        [ sp.cos(gamma), 0, sp.sin(gamma)],
-        [ 0,             1, 0            ],
+        [sp.cos(gamma), 0, sp.sin(gamma)],
+        [0,             1, 0],
         [-sp.sin(gamma), 0, sp.cos(gamma)]
     ])
+
+
+R = sp.Matrix([
+    [sp.cos(q1 + q1),  -sp.sin(q1+q2), 0],
+    [-sp.sin(q1 + q2),  sp.cos(q1 + q2), 0],
+    [0,  0,  1]
+])
